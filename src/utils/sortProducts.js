cc391d7dfc86fc8d6731ed09ca438ec0f9a0b226
Janeil Tan <janeil.tan@developersinstitute.co.nz>
2023-03-23 22:23:44 +1300

    Coded along to update function in sortProducts.js

--- src/utils/sortProducts.js
+++ src/utils/sortProducts.js
@@ -1,5 +1,22 @@
 const sortProducts = (products, sortBy) => {
   console.log("TODO! Sort products by:", sortBy);
+
+  products.sort((productA, productB) => {
+    const aPrice = productA.prices[0].unit_amount;
+    const bPrice = productB.prices[0].unit_amount;
+
+    if (sortBy === "low") {
+      return aPrice - bPrice;
+    }
+
+    if (sortBy === "high") {
+      return bPrice - aPrice;
+    }
+    if (sortBy === "latest") {
+      return productB.created - productA.created;
+    }
+  });
+
   return products;
 };
 
